3b450bf3b1b1426260e328828726b277773dd9f3
bp605802 <pierre.bonny@etu.unice.fr>
2016-11-14 18:23:09 +0100

    ajout s7

--- Bfck.java
+++ Bfck.java
@@ -24,18 +24,31 @@ public class Bfck {
         Output output = new Output(memory);
         Interpreter interpreter = new Interpreter(computational);
         Reader reader=new Reader(interpreter,output);
+        Translatetoimage translatetoimage = new Translatetoimage();
+        Translatetointruct translatetointruct = new Translatetointruct();
 
         while(i!=nbArgs-1){
         	if(args[i].equals("--rewrite")){
-        		reader.rewrite(args[nbArgs-1]);
+                if (args[nbArgs -1].contains("BMP")||args[nbArgs -1].contains("bmp")){
+                    translatetointruct.rewriteImage(args[nbArgs - 1]);
+                }
+                else{
+                    reader.rewrite(args[nbArgs-1]);
+                }
+
         	}
         	else if (args[i].equals("-translate")){
-                    Picture picture = new Picture();
+
                     FileReader file = new FileReader(args[nbArgs-1]);
-                    picture.translate(file);
+                    translatetoimage.translate(file);
                 }
-        	else if (args[i].equals("-p")){    
-                reader.read(args[nbArgs-1]);
+        	else if (args[i].equals("-p")){
+                if (args[nbArgs -1].contains("BMP")||args[nbArgs -1].contains("bmp")){
+                    interpreter.interpreteImage(args[nbArgs - 1]);
+                }
+                else {
+                    reader.read(args[nbArgs-1]);
+                }
                 }
             i++;
         }       
