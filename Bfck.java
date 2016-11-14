7e131706f3f91241eb37fd75287f49fa871565fd
Thibault.Larzabal <thibault.larzabal@gmail.com>
2016-11-14 19:46:49 +0100

    e

--- Bfck.java
+++ Bfck.java
@@ -24,20 +24,28 @@ public class Bfck {
         Output output = new Output(memory);
         Interpreter interpreter = new Interpreter(computational);
         Reader reader=new Reader(interpreter,output);
+        boolean sansOption=true;
 
         while(i!=nbArgs-1){
         	if(args[i].equals("--rewrite")){
         		reader.rewrite(args[nbArgs-1]);
+                sansOption=false;
         	}
         	else if (args[i].equals("-translate")){
                     Picture picture = new Picture();
                     FileReader file = new FileReader(args[nbArgs-1]);
                     picture.translate(file);
+                    sansOption=false;
                 }
-        	else if (args[i].equals("-p")){    
+            else if (args[i].equals("-i") || args[i].equals("-o")){
+                
                 reader.read(args[nbArgs-1]);
-                }
+                sansOption=false;
+            }
             i++;
+        }
+        if (sansOption){    
+            reader.read(args[nbArgs-1]);
         }       
     }       
 }
